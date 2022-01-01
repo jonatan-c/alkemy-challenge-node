676686b10e8ec9f89dd361f214646c7c39752b6e
jonatan <jona.cc.93@gmail.com>
2022-01-01 09:20:11 -0300

    [add]base de datos y modelos

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./models/Relations");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+db.sequelize.sync().then(() => {
+  console.log("DB has been created successfully.");
+});
 
 //***********************SWAGGER**********************/
 const swaggerUI = require("swagger-ui-express");
